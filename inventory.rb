f511fca946839024e8b0742aca262e0e4c5447fd
Kevin Rocker <kevin.j.rocker@gmail.com>
2016-07-20 16:28:46 -0400

    Add command parser

--- inventory.rb
+++ inventory.rb
@@ -15,12 +15,23 @@ def show(inventory)
   # Display inventory
 end
 
+def command_parser(input)
+  valid_commands = ["help","show","add","update","remove"]
+  command, *center_arg, final_arg = input
+  if valid_commands.include?(command)
+    return "I know what to do with this command.\nIt's the '#{command}' command,
+            with arguments #{center_arg} and #{final_arg}."
+  else
+    return "I don't know what to do with the #{command} command."
+  end
+end
+
 inventory = {}
 command = ""
 
 while command != 'close'
   print '#> '
   input = gets.chomp.split(' ')
+  puts command_parser(input)
   command = input[0]
-  puts "I'm going to run the #{command} command here."
 end
