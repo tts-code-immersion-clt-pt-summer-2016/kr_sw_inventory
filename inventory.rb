8922b3b7172c5ae1181863b87a97a379a6e40b65
Stacy White <whitestacy8@gmail.com>
2016-07-24 21:40:50 -0400

    SW file

--- inventory.rb
+++ inventory.rb
@@ -1,61 +1,43 @@
+class Inventory
 
-def shift(inventory, item, amount)
+  attr_reader :item, :amount
 
-end
-
-def update(inventory, item, new_amount)
-  # Update amount of item in inventory
-end
-
-def remove(inventory, item)
-  # Remove item from inventory
-end
-
-def add(inventory, item, amount)
-  # Add item to inventory
-end
-
-def show(inventory)
-  # Display inventory
-end
-
-def command_caller(command, args)
-  *item, value = args
-  item_name = center_args.join(' ').lowercase
-  case command
-  when "help"
-    # Run help method
-  when "new"
-    # Create new inventory item
-  when "update"
-    # Set new inventory value
-  when "delete"
-    # Remove an item from inventory
-  when "add", "subtract"
-    # Update item amount based on existing amount
-  else
-    # Something invalid got through.
-    return false
+  def initialize(item,amount)
+    @item=item
+    @amount=amount
   end
-  return true
-end
 
-def command_parser(input)
-  valid_commands = ["help","new","update","delete","add","subtract","close"]
-  command, *arguments = input
-  if valid_commands.include?(command)
-    return "I know what to do with this command.\nIt's the '#{command}' command."
-  else
-    return "I don't know what to do with the #{command} command."
+  def new(item)
+    inv
+  end
+
+  def update(amount)
+    #set an item amount
+  end
+
+  def add(amount)
+    #add amount to item
+  end
+
+  def Remove(amount)
+    #remove amount from item
+  end
+
+  def delete(item)
+    #delete item entirely
+  end
+
+  def help
+    #help
+  end
+
+  def Display(item,amount)
+    #show item and amount
+  end
+
+  def close
+    #close
   end
 end
 
-inventory = {}
-command = ""
-
-while command != 'close'
-  print 'Inventory #> '
-  input = gets.chomp.split(' ')
-  puts command_parser(input)
-  command = input[0]
-end
+inv = {}
